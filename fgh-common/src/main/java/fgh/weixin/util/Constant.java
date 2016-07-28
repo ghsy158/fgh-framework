c151fb0d3a7c1be0d53fc46990b2de30e571aec1
ghsy158 <fgh326@126.com>
2016-07-28 15:24:48 +0800

    常量类

---
+++ fgh-common/src/main/java/fgh/weixin/util/Constant.java
@@ -0,0 +1,24 @@
+package fgh.weixin.util;
+
+/**
+ * 常量类
+ * 
+ * @author fgh
+ * @since 2016年7月28日上午11:20:06
+ */
+public class Constant {
+
+	/**
+	 * 请求方法
+	 * 
+	 * @author fgh
+	 * @since 2016年7月28日上午11:21:41
+	 */
+	public class requestMethod {
+		public static final String GET = "GET";
+		public static final String POST = "POST";
+	}
+
+	public static final String REDIS_CORP_TOKEN_KEY = "corpToken";
+	public static final String REDIS_CORP_TOKEN_EXPIRE_KEY = "corpTokenExpire";
+}
